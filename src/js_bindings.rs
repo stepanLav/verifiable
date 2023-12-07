56409f100f91a04abe49f59757e39a6fc69d22ea
peetzweg/ <phil.czek@gmail.com>
2023-12-07 20:16:23 +0000

    wip js bindings for `BandersnatchVrfVerifiable`

--- src/js_bindings.rs
+++ src/js_bindings.rs
@@ -1,15 +1,59 @@
 
+use ark_scale::ArkScale;
+use bandersnatch_vrfs::PublicKey;
 use wasm_bindgen::prelude::*;
 
-use crate::demo_impls::Trivial;
+use crate::{demo_impls::Trivial, ring_vrf_impl::BandersnatchVrfVerifiable};
 use crate::GenerateVerifiable;
-use js_sys::{Array, Object, Uint8Array};
+use js_sys::{Array, Object, Uint8Array, Boolean};
 
 
+fn to_u8_32(input:Uint8Array) -> [u8; 32] {
+	let mut entropy = [0u8; 32];
+	entropy.copy_from_slice(&input.to_vec());
+	entropy
+}
+
+// fn to_u8_32_chunks(input: Uint8Array) -> impl Iterator<Item = [u8; 32]> {
+//     input.clone().to_vec().clone().chunks(32).map(|chunk| {
+//             let mut m = [0u8; 32];
+//             m.copy_from_slice(&chunk);
+//             m
+//         }).clone().to_owned()
+// }
 
 #[wasm_bindgen]
-pub fn new_array() -> Uint8Array {
-    Uint8Array::new(&JsValue::from("Hello Array!"))
+pub fn all(entropy: Uint8Array, members:Uint8Array) -> Object {
+	let entropy = to_u8_32(entropy);
+
+	// Get Secret from Entropy
+	let secret = BandersnatchVrfVerifiable::new_secret(entropy);
+
+	// Get Member from Secret
+	let member = BandersnatchVrfVerifiable::member_from_secret(&secret);
+
+	let raw_members = members.to_vec();
+    let members_iter =
+        raw_members.chunks(32).map(|chunk| {
+			let reader = &mut &chunk[..];
+			// this fails for now :S
+			// ArkScale::from(PublicKey::deserialize(reader).expect("Invalid public key"))
+			1
+		});
+
+
+
+	// // Get Commitment from Member
+	// let commitment = BandersnatchVrfVerifiable::open(&member, members_iter).expect("Error during open");
+
+	// // Get Proof & Alias from Commitment
+	// let (proof,alias) = BandersnatchVrfVerifiable::create(commitment, &secret, &[23u8], &[42u8]).expect("Error during open");
+
+	let obj = Object::new();
+	js_sys::Reflect::set(&obj, &"proof".into(), &Boolean::from(true)).unwrap();
+	// js_sys::Reflect::set(&obj, &"proof".into(), &Uint8Array::from(&proof[..])).unwrap();
+	// js_sys::Reflect::set(&obj, &"alias".into(), &Uint8Array::from(&alias[..])).unwrap();
+	obj
 }
 
 #[wasm_bindgen]
@@ -19,12 +63,17 @@ pub fn new_secret(entropy_input: Uint8Array) -> Uint8Array {
 
     let secret = Trivial::new_secret(entropy);
 
-    // create Uint8array from secret
+	let secret_snatch = BandersnatchVrfVerifiable::new_secret(entropy);
 
-    let mut secret_bytes = [0u8; 32];
-    secret_bytes.copy_from_slice(&secret.to_vec()[..]);
+	let public = secret_snatch.as_publickey();
+	if public.0.is_on_curve() {
+		let mut secret_bytes = [0u8; 32];
+		secret_bytes.copy_from_slice(&secret.to_vec()[..]);
 
-    Uint8Array::from(&secret_bytes[..])
+		Uint8Array::from(&secret_bytes[..])
+	}else{
+		Uint8Array::new(&JsValue::from("value is not on curve"))
+	}
 }
 
 #[wasm_bindgen]
@@ -43,12 +92,12 @@ pub fn member_from_secret(secret_input: Uint8Array) -> Uint8Array {
 
 #[wasm_bindgen]
 pub fn open(member_input: Uint8Array, members_input: Uint8Array) -> Object {
+	// Handle Input
     let mut member = [0u8; 32];
 
     member.copy_from_slice(&member_input.to_vec());
 
     let raw_members = members_input.to_vec();
-
     let members_iter =
         raw_members.chunks(32).map(|chunk| {
             let mut m = [0u8; 32];
@@ -56,23 +105,24 @@ pub fn open(member_input: Uint8Array, members_input: Uint8Array) -> Object {
             m
         });
 
+	// Actual Function Call
     let result = Trivial::open(&member, members_iter);
+
+	// Handle Output
     let output = match result {
         Ok((me, members)) => {
-            let obj = Object::new();
 
-            let mut me_tmp = [0u8; 32];
-            me_tmp.copy_from_slice(&me.to_vec()[..]);
-            let me = Uint8Array::from(&me_tmp[..]);
+
+
+            let me = Uint8Array::from(&me[..]);
 
             let array = Array::new();
             for member in members.iter() {
-                let mut member_tmp = [0u8; 32];
-                member_tmp.copy_from_slice(&member.to_vec()[..]);
-                let member = Uint8Array::from(&member_tmp[..]);
+                let member = Uint8Array::from(&member[..]);
                 array.push(&member);
             }
 
+			let obj = Object::new();
             js_sys::Reflect::set(&obj, &"me".into(), &me).unwrap();
             js_sys::Reflect::set(&obj, &"members".into(), &array).unwrap();
             obj
